6543f27c10e957664517d2da780b7e563e52cbdf
Max-shiri-90 <max.shiri.90@gmail.com>
2023-03-08 23:14:01 +0330

    admin

--- online-shop/accounts/admin.py
+++ online-shop/accounts/admin.py
@@ -5,5 +5,4 @@ from accounts.models import Customer, Supplier, CustomerWallet
 
 admin.site.register(Customer)
 admin.site.register(Supplier)
-# admin.site.register(Address)
 admin.site.register(CustomerWallet)
