2c147b1c6958f300c2f26527a7a73545dee73c2e
Max-shiri-90 <max.shiri.90@gmail.com>
2023-03-12 21:06:04 +0330

    setings

--- online-shop/ecommerce/settings.py
+++ online-shop/ecommerce/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [  # byd bashe ta masiro to url beshnace
+STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
